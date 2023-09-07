4878c8a71c19ee51d91571131cf2967d6cbefab9
Sumukh Prasad <sumukhprasad@icloud.com>
2023-09-08 04:32:18 +0530

    Added build-ctan-dist.sh

---
+++ build-ctan-dist.sh
@@ -0,0 +1,23 @@
+
+
+echo "\033[1mCreating directory...\033[0m"
+mkdir ../ctan-dist
+
+echo "\033[1mCopying files...\033[0m"
+rsync -av --exclude=".*" ./* "../ctan-dist/"
+
+mv ../ctan-dist ./ctan-dist
+cd ctan-dist
+
+mv main.tex curriculum-vitae-example.tex
+mv main.pdf curriculum-vitae-example.pdf
+rm -rf .git
+rm -rf __MACOSX
+rm build.sh
+rm build-ctan-dist.sh
+
+cd ..
+
+mv ctan-dist curriculum-vitae
+zip -vr ctan-dist.zip ctan-dist/ -x "*.DS_Store"
+mv curriculum-vitae ctan-dist
